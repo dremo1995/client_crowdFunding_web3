e0c25064866ea582ee2a04ec1b7440db85451971
Andre <asm13august95@gmail.com>
2023-01-09 14:11:16 -0600

    create navbar, sidebar and general set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { ReactDOM } from "react-dom/client";
+import { BrowserRouter as Router } from 'react-router-dom'
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
+    <ThirdwebProvider desiredChainId={ ChainId.Goerli }>
+      <Router>
+        <App />
+      </Router>
     </ThirdwebProvider>
   </React.StrictMode>
 );
